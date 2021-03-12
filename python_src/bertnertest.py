5685defab01a2f250b8e71f0a90ccaa7eb4791f0
KareemAlaa2001 <kareemalaa2001@gmail.com>
2021-03-12 19:10:21 +0000

    created method to pner strings and put some documentation for FVs

--- python_src/bertnertest.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModel  
-
-tokenizer = AutoTokenizer.from_pretrained("lanwuwei/BERTOverflow_stackoverflow_github")  
-model = AutoModel.from_pretrained("lanwuwei/BERTOverflow_stackoverflow_github")
--- python_src/textner.py
+++ python_src/textner.py
@@ -1,9 +1,24 @@
-# import extraction
+import extraction
 # from extraction import recentPosts
 import stanza
 import re
 from stanza.server import CoreNLPClient
+import numpy as np
 # print(extraction.recentUsers[0])
+
+"""
+For English, by default, this annotator recognizes named (PERSON, LOCATION, ORGANIZATION, MISC), 
+numerical (MONEY, NUMBER, ORDINAL, PERCENT), and temporal (DATE, TIME, DURATION, SET) entities (12 classes).
+Adding the regexner annotator and using the supplied RegexNER pattern files adds support for the fine-grained and
+additional entity classes EMAIL, URL, CITY, STATE_OR_PROVINCE, COUNTRY, NATIONALITY, RELIGION, (job) TITLE, 
+IDEOLOGY, CRIMINAL_CHARGE, CAUSE_OF_DEATH, (Twitter, etc.) HANDLE (12 classes) for a total of 24 classes. 
+
+"""
+
+entityTypes = ["PERSON", "LOCATION", "ORGANIZATION", "MISC", "MONEY", "NUMBER", 
+    "ORDINAL", "PERCENT", "DATE", "TIME", "DURATION", "SET", "EMAIL", "URL", "CITY", 
+    "STATE_OR_PROVINCE", "COUNTRY", "NATIONALITY", "RELIGION", "TITLE", "IDEOLOGY", 
+    "CRIMINAL_CHARGE", "CAUSE_OF_DEATH", "HANDLE"]
 
 class NerProcessor:
 
@@ -31,14 +46,15 @@ def main():
     # print(*[f'entity: {ent.text}\ttype: {ent.type}' for sent in testNerObj.sentences for ent in sent.ents], sep='\n')
 
     with CoreNLPClient(
-        annotators=['tokenize','ssplit','pos','lemma','ner'],
-        timeout=30000,
-        memory='16G', classpath="../Libraries/stanford-corenlp-4.2.0/") as client:
+            annotators=['tokenize','ssplit','pos','lemma','ner'],
+            timeout=30000,
+            memory='16G') as client:
 
         ann = client.annotate(text)
-        sentence = ann.sentence[0]
-        entities = sentence.nerTags
-        print(entities)
+        sentences = ann.sentence
+        for sent in sentences:
+            for token in sent.token:
+                print(token.word, token.ner)
 
 
     # print(testNerObj)
@@ -50,18 +66,95 @@ def main():
     #     print(cleanXML(samplePost['Body']))
     #     print('\n')
 
-def basicNer(str):
-    nlp = stanza.Pipeline(lang='en', processors='tokenize,ner')
-    return nlp(str)
 
 def cleanXML(string):
     return re.sub('<.*?>', '', string)
 
-def convertStringToNER(string):
+"""
+WILL NOTE THE PROPERTIES OF ALL THE DIFFERENT NODE TYPES HERE:
+
+POST: 
+
+ Example post:
+    row Id="250001" 
+    PostTypeId="1" 
+    AcceptedAnswerId="250002" 
+    CreationDate="2014-04-17T00:49:28.617" 
+    Score="21" ViewCount="893" 
+    Body="&lt;p&gt;Will we get our reputation moved over or questions?&lt;/p&gt;&#xA;"
+    OwnerUserId="23528" 
+    LastEditorUserId="387076" 
+    LastEditDate="2014-04-17T00:51:26.693" 
+    LastActivityDate="2014-04-17T01:36:58.283" 
+    Title="Will the questions be migrated over from meta.stackexchange.com?" 
+    Tags="&lt;discussion&gt;&lt;meta&gt;&lt;mso-mse-split&gt;" 
+    AnswerCount="4" 
+    CommentCount="6" 
+    ContentLicense="CC BY-SA 3.0"
+
+ Useful stuff for links:
+    Id
+    AcceptedAnswerId
+    OwnerUserId
+    (LastEdifotrUserId)??
+
+ Useful stuff for an FV: 
+    Creationdate - convert to timestamp
+    Score
+    ViewCount
+    BodyNER
+    (LastEditDate)??
+    (LastActivityDate)??
+    TitleNER
+    Tags [Need to build a vector for that]
+    AnswerCount
+    CommentCount
+
+"""
+# WILL GO WITH THE IDEA OF HAVING NER SLOTS IN COMMON THEN HAVING EXTRA SLOTS FOR DIFFERENT METADATA. 
+
+"""
+FEATUREVECTOR DOCUMENTATION:
+
+This will keep track of what each slot in the FV corresponds to.
+
+
+"""
+
+def postToFV(post):
+    fv = [] # need to decide on the structure for a general node FV, then use that here
+
+"""
+entityTypes = ["PERSON", "LOCATION", "ORGANIZATION", "MISC", "MONEY", "NUMBER", 
+    "ORDINAL", "PERCENT", "DATE", "TIME", "DURATION", "SET", "EMAIL", "URL", "CITY", 
+    "STATE_OR_PROVINCE", "COUNTRY", "NATIONALITY", "RELIGION", "TITLE", "IDEOLOGY", 
+    "CRIMINAL_CHARGE", "CAUSE_OF_DEATH", "HANDLE"]
+
+"""
+
+# THIS FUNCTION CAN BE EXPANDED TO INCLUDE ANY FUTURE ADDITIONAL PREPROCESSING NEEDED FOR THE STRING
+def convertStringToNER(string, client):
     string = cleanXML(string)
-    # need to add in funcionality to take that and convert to a NER frequency vector
-    # first need to be able to customise entity types in the stanza pipeline
-    return string
+    # need to add in functionality to take that and convert to a NER frequency vector
+    ann = client.annotate(string)
+    nerVector = buildNERVector(ann)
+    return nerVector
+
+def buildNERVector(ann):
+    nerVector = np.zeros(24)
+    sentences = ann.sentence
+
+    for sent in sentences:
+        tokens = sent.token
+        for tok in tokens:
+            if tok.ner != 'O':
+                index = entityTypes.index(tok.ner)
+                nerVector[index] += 1
+
+    return nerVector
+
+
+    
 
 if __name__ == '__main__':
     main()
